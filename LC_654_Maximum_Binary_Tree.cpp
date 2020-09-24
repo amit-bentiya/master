f5cffe84066ab7c1803740651029b4518787bd70
Amit Bentiya <71172534+amit-bentiya@users.noreply.github.com>
2020-09-24 22:46:03 +0530

    Update LC_654_Maximum_Binary_Tree.cpp

--- LC_654_Maximum_Binary_Tree.cpp
+++ LC_654_Maximum_Binary_Tree.cpp
@@ -37,7 +37,22 @@ Output: return the tree root node representing the following tree:
 class Solution {
 public:
     TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        
+        stack<TreeNode*> stk;
+        for (int num : nums) {
+            TreeNode* node = new TreeNode(num);
+            while (!stk.empty() && stk.top() -> val < node -> val) {
+                node -> left = stk.top();
+                stk.pop();
+            }
+            if (!stk.empty()) {
+                stk.top() -> right = node;
+            }
+            stk.push(node);
+        }
+        while (stk.size() > 1) {
+            stk.pop();
+        }
+        return stk.top();        
     }
 };
 
